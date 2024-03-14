3f7ffd3541e47229d631020b24a143207170827c
areseius <areseius@gmail.com>
2024-03-14 15:03:49 +0400

    new task added

---
+++ tasks/march 2024/task5 15.03.2024/script.js
@@ -0,0 +1,89 @@
+//1 1-dən 100-ə qədər olan bütün ədədləri ekrana çap edin.
+//2 1-dən 100-ə qədər olan bütün cütləri ekrana çap edin.
+//3 1-dən 100-ə qədər olan bütün tək ədədlərin cəmini tapın və ekrana çap edin.
+//4 1-dən 100-ə qədər olan bütün ədədlərin kvadratlarını ekrana çap edin.
+//5 1-dən 100-ə qədər olan bütün ədədlərin kublarını ekrana çap edin.
+//6 1-dən 100-ə qədər olan bütün cütlərin cəmini tapın və ekrana çap edin.
+//7 1-dən 100-ə qədər olan bütün tək ədədlərin hasilini tapın və ekrana çap edin.
+//8 1-dən 100-ə qədər olan bütün cütlərin hasilini tapın və ekrana çap edin.
+//9 1-dən 100-ə qədər olan bütün ədədlərin toplamını tapın və ekrana çap edin.
+//10 1-dən 100-ə qədər olan bütün ədədlərin orta qiymətini tapın və ekrana çap edin.
+
+// *task1
+
+// const showNums = () => {
+//   for (let i = 1; i <= 100; i++) console.log(i);
+// };
+
+// showNums();
+
+// *task2
+
+// const showEvenNums = () => {
+//   for (let i = 2; i <= 100; i += 2) console.log(i);
+// };
+
+// showEvenNums();
+
+// *task3
+
+// ededi silsile cem dusturu : (((a1 + an) / 2) * (an - a1 + d)) / d
+
+// const calculateOddNums = (a1, an, d) => console.log((((a1 + an) / 2) * (an - a1 + d)) / d);
+
+// calculateOddNums(1, 99, 2);
+
+// *task4
+
+// const calculateNumsSquared = () => {
+//   for (let i = 1; i <= 100; i++) console.log(i ** 2);
+// };
+
+// calculateNumsSquared();
+
+// *task5
+
+// const calculateNumsCubed = () => {
+//   for (let i = 1; i <= 100; i++) console.log(i ** 3);
+// };
+
+// calculateNumsCubed();
+
+// *task6
+
+// const calculateOddNums = (a1, an, d) => console.log((((a1 + an) / 2) * (an - a1 + d)) / d);
+
+// calculateOddNums(2, 100, 2);
+
+// *task7
+
+// const calculateMultiplyOddNums = () => {
+//   for (var i = 1, result = 1; i <= 100; i += 2) result *= i;
+//   console.log(result);
+// };
+
+// calculateMultiplyOddNums();
+
+// *task8
+
+// const calculateMultiplyEvenNums = () => {
+//   for (var i = 2, result = 1; i <= 100; i += 2) result *= i;
+//   console.log(result);
+// };
+
+// calculateMultiplyEvenNums();
+
+// *task9
+
+// const calculateAddNums = (a1, an, d) => console.log((((a1 + an) / 2) * (an - a1 + d)) / d);
+
+// calculateAddNums(1, 100, 1);
+
+// *task10
+
+// const calculateMultiplyEvenNums = () => {
+//   for (var i = 1, sum = 0; i <= 100; i++) sum += i;
+//   console.log(sum / 100);
+// };
+
+// calculateMultiplyEvenNums();
